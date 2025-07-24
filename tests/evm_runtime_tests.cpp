e7718891c131fa8144230917222f9ae503faa567
kayan <taokayan13@gmail.com>
2025-07-24 19:58:28 +0800

    fix unittest compile error with spring 2.0

--- tests/evm_runtime_tests.cpp
+++ tests/evm_runtime_tests.cpp
@@ -471,7 +471,7 @@ struct evm_runtime_tester : eosio_system_tester, silkworm::State {
       const auto& accnt = control->db().get<account_object,by_name>(ME);
       abi_def abi;
       BOOST_REQUIRE_EQUAL(abi_serializer::to_abi(accnt.abi, abi), true);
-      evm_runtime_abi.set_abi(abi, abi_serializer_max_time);
+      evm_runtime_abi.set_abi(abi, {} /*yield_function_t*/);
 
       base_tester::push_action(ME,
                                "init"_n,
